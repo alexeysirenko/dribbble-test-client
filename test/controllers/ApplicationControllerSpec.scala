ffd457bbdb5c1ce8e6031913997cb8fa354cea98
Alexey Sirenko <drunk.pacifist@gmail.com>
2017-10-18 22:46:34 +0300

    Add integration tests

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -2,44 +2,21 @@ package controllers
 
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
+import play.api.libs.ws.WSClient
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- *
- * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
- */
-class ApplicationControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+class ApplicationControllerSpec extends PlaySpec with GuiceOneServerPerSuite with Injecting {
 
-  "HomeController GET" should {
+  "test server logic" in {
+    val wsClient = app.injector.instanceOf[WSClient]
+    val myPublicAddress = s"localhost:$port"
+    val username = "KreativaStudio"
+    val url = s"http://$myPublicAddress/top10?login=$username"
+    // await is from play.api.test.FutureAwaits
+    val response = await(wsClient.url(url).get())
 
-    "render the index page from a new instance of controller" in {
-      val controller = new ApplicationController(stubControllerComponents())
-      val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
-    "render the index page from the application" in {
-      val controller = inject[ApplicationController]
-      val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
-    "render the index page from the router" in {
-      val request = FakeRequest(GET, "/")
-      val home = route(app, request).get
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
+    response.status mustBe OK
   }
+
 }
